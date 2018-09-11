ff544f1502409e0c88e8cfcafad3e2260c64afaf
Nunezjoaquin98 <40808656+Nunezjoaquin98@users.noreply.github.com>
2018-09-10 22:09:31 -0300

    Add files via upload

---
+++ clase 6/Untitled1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+main()
+{
+    char nombre[20] = {"MARIANa"} ;
+    char apellido[20]  = {"PeREz"} ;
+    char espacio[2] = {" "} ;
+    char nombreCompleto[40] ;
+
+    strcmp(nombreCompleto,nombre) ;
+    printf("%s",nombreCompleto) ;
+    return 0;
+}
--- clase 6/main.c
+++ clase 6/main.c
@@ -1,14 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-void mostrarCharArrays (char vec[],int tam) ;
+#include <string.h>
+void mostrarCharArrays(char vec[],int tam) ;
+void ordenarArrays (char vec[],int tam) ;
+void ordenarArraysConCriterio (char vec[],int tam,int criterio) ;
+void nombreApellidoEnCapital(char nombre[]) ;
 
 int main()
 {
-   char letras[] = {'d','c','a','f','A','c','z','Z'} ;
+    /*
+   char letras[] = {'B','F','G','A','C','D','H','I'} ;
 
+   ordenarArraysConCriterio(letras,8,1);
    mostrarCharArrays(letras,8) ;
 
-   return 0;
+    return 0;
+    */
+
+    char nombre[20] = {"MARIANa"} ;
+    char apellido[20]  = {"PeREz"} ;
+    char nombreCompleto[40] ;
+
+    strcpy(nombreCompleto,nombre) ;
+    strcat(nombreCompleto," ") ;
+    strcat(nombreCompleto,apellido) ;
+    strlwr(nombreCompleto) ;
+
+    printf("%s",nombreCompleto) ;
+}
+
+void nombreApellidoEnCapital(char nombre[])
+{
+    int cant = strlen(nombre) ;
+
+    for(int i = 0 ; i < cant ; i++)
+    {
+        if(nombre[i+1]= toupper(nombre[i+])) ;
+    }
 }
 
 void mostrarCharArrays(char vec[],int tam)
@@ -19,3 +47,63 @@ void mostrarCharArrays(char vec[],int tam)
     }
 
 }
+
+
+
+void ordenarArrays (char vec[],int tam)
+{
+    int aux;
+
+    for(int i = 0 ; i < tam-1 ; i++)
+    {
+        for(int j = i + 1; j < tam ; j++)
+        {
+            if(vec[i]>vec[j])
+            {
+                aux = vec[i] ;
+                vec[i]= vec[j] ;
+                vec[j]= aux ;
+            }
+        }
+    }
+
+}
+
+
+void ordenarArraysConCriterio (char vec[],int tam,int criterio)
+{
+    int aux;
+if(criterio)
+{
+
+    for(int i = 0 ; i < tam-1 ; i++)
+    {
+        for(int j = i + 1; j < tam ; j++)
+        {
+            if(vec[i]>vec[j])
+            {
+                aux = vec[i] ;
+                vec[i]= vec[j] ;
+                vec[j]= aux ;
+            }
+        }
+    }
+    }
+else
+    {
+
+    for(int i = 0 ; i < tam-1 ; i++)
+    {
+        for(int j = i + 1; j < tam ; j++)
+        {
+            if(vec[i]<vec[j])
+            {
+                aux = vec[i] ;
+                vec[i]= vec[j] ;
+                vec[j]= aux ;
+            }
+        }
+    }
+    }
+}
+
