f73ebe3f05b60897c0c482503ed97acad52cad15
Nunezjoaquin98 <40808656+Nunezjoaquin98@users.noreply.github.com>
2019-05-28 21:10:08 -0300

    Add files via upload

---
+++ clase 19 xd/Practica_Pair_v1.3/Practica_Pair_v1.2/Windows_64/Employee.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+
+int employee_compare(void* pEmployeeA,void* pEmployeeB)
+{
+    return 0;
+}
+
+
+void employee_print(Employee* this)
+{
+
+}
+
+
+Employee* employee_new(void)
+{
+
+    Employee* returnAux = NULL;
+
+    return returnAux;
+
+}
+
+void employee_delete(Employee* this)
+{
+
+
+}
+
+int employee_setId(Employee* this, int id)
+{
+    int ret;
+
+    if(id > 0)
+    {
+        this->id = id;
+    }else
+    {
+        ret = -1;
+    }
+
+    return ret;
+}
+
+int employee_getId(Employee* this)
+{
+
+    return this->id;
+
+}
+
+int employee_setName(Employee* this, char name)
+{
+
+    this->name = name;
+
+
+    return 0;
+}
+
+int employee_setlastName(Employee* this, char lastName)
+{
+
+    this->lastName = lastName;
+
+
+    return 0;
+}
+
---
+++ clase 19 xd/Practica_Pair_v1.3/Practica_Pair_v1.2/Windows_64/parser.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+
+int parserEmployee(FILE* pFile, Employee* pArrayListEmployee)
+{
+    int r,i=0;
+    char auxId[5];
+    char name[51];
+    char lastName[51];
+    char auxIsEmpty[6];
+    pFile = fopen("data.csv","r");
+
+    if(pFile == NULL)
+    {
+        printf("El archivo no existe");
+        exit(EXIT_FAILURE);
+    }
+    do
+    {
+
+    r = fscanf(pFile,"%[^,],%[^,],%[^,],%[^\n]\n",auxId,name,lastName,auxIsEmpty);
+    //employee_setId(pArrayListEmployee[i],atoi(auxId));
+    strncpy(pArrayListEmployee[i].name,name,strlen(name));
+    strncpy(pArrayListEmployee[i].lastName,lastName,strlen(lastName));
+    if(auxIsEmpty == "true")
+    {
+      pArrayListEmployee[i].isEmpty = 1;
+    }else
+    {
+      pArrayListEmployee[i].isEmpty = 0 ;
+    }
+
+    if(r == '4')
+    {
+        printf("%d  %s  %s  %d\n",pArrayListEmployee[i].id,pArrayListEmployee[i].name,pArrayListEmployee[i].lastName,pArrayListEmployee[i].isEmpty);
+    }
+
+    }while(!feof(pFile));
+
+    fclose(pFile);
+
+
+
+    return 0;
+}
